1d5bc341a4bc2e9f4a35640369e4251aba12023e
15ags <15ags@proton.me>
2026-05-30 20:37:54 -0600

    test-tarea-2

---
+++ src/main/java/frc/robot/interfaces/ArmIO.java
@@ -0,0 +1,25 @@
+package frc.robot.interfaces;
+
+import com.stzteam.mars.models.singlemodule.Data;
+import com.stzteam.mars.models.singlemodule.IO;
+
+public interface ArmIO extends IO<ArmIO.ArmInputs> {
+  public static class ArmInputs extends Data<ArmInputs> {
+    public double currentAngle = 0.0;
+    public double targetAngle = 0.0;
+    public boolean isAtBottom = false;
+    public boolean isAtTop = false;
+    public double appliedVoltage = 0.0;
+    public double appliedCurrent = 0.0;
+  }
+
+  public void moveWithVoltage(double voltage);
+
+  public void moveWithDutyCycle(double dutyCycle);
+
+  public void moveToPos(double pos);
+
+  public void stopMotor();
+
+  public void calibratePos();
+}
---
+++ src/main/java/frc/robot/interfaces/TurretIO.java
@@ -0,0 +1,25 @@
+package frc.robot.interfaces;
+
+import com.stzteam.mars.models.singlemodule.Data;
+import com.stzteam.mars.models.singlemodule.IO;
+
+public interface TurretIO extends IO<TurretIO.TurretInputs> {
+  public static class TurretInputs extends Data<TurretInputs> {
+
+    public double currentAngle = 0.0;
+    public double targetAngle = 0.0;
+    public double currentRps = 0.0;
+    public double appliedVoltage = 0.0;
+    public double appliedCurrent = 0.0;
+  }
+
+  public void moveWithVoltage(double voltage);
+
+  public void moveWithDutyCycle(double dutyCycle);
+
+  public void moveToPos(double pos);
+
+  public void stopMotor();
+
+  public void resetEncoder();
+}
